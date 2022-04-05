a16866bef6ee7cef85be154b83e03a5b5425f919
j-33-t <prasenjeet.rathore@gmail.com>
2022-04-05 20:57:53 +0200

    final-5_april_2022

--- chapter_11_Project_2_Document.py
+++ chapter_11_Project_2_Document.py
@@ -15,30 +15,57 @@ WebCam.set(10,100) #brightness has id no.10
 
 
 def preProcessing(img):
-    # 1. Converting to greyscale 
-    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
-    # 2. Apply Blur
+    imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     imgBlur = cv.GaussianBlur(imgGray,(5,5),1)
-    
-    # Find Edges
-    imgCanny = cv.Canny(imgBlur, 200,200)
-    
-    # Dilation and Erosion
+    imgCanny = cv.Canny(imgBlur,200,200)
     kernel = np.ones((5,5))
-    imgDialation = cv.dilate(imgCanny, kernel , iterations = 2)
-    imgThreshold = cv.erode(imgDialation, kernel, iterations = 1)
-    
-    return imgThreshold
+    imgDial = cv.dilate(imgCanny,kernel,iterations=2)
+    imgThres = cv.erode(imgDial,kernel,iterations=1)
+    return imgThres
 
+
+def getContours(img):
+    biggest = np.array([])
+    maxArea = 0
+    contours,hierarchy = cv.findContours(img,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        if area > 5000:
+            #cv.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv.arcLength(cnt,True)
+            approx = cv.approxPolyDP(cnt,0.02*peri,True)
+            if area >maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
+    return biggest
+
+
+def getWarp(img, biggest):
+    
+    pass
 
 while True:
     success, img = WebCam.read()
     
     # Resize width and height of img
     cv.resize(img, (widthImg, heightImg))
-    # Show result
-    cv.imshow("WEBCAM", img)
+    
+    # Copy img 
+    imgContour = img.copy()
+    
+    # Adding imgThreshold
+    imgThres = preProcessing(img)
+    
+    # Adding contour to imgThreshold
+    biggest = getContours(imgThres)
+    getWarp(img,biggest)
+    
+    # Flip image
+    imgThres = cv.flip(imgThres,1)
+    
+    # Show results
+    cv.imshow("Result", imgThres)
     
     # Add Delay and Key to Break loop
     if cv.waitKey(33) & 0xFF==ord("q"):
