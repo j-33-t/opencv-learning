301b262acddd7bcd7e0fc221670c4eff22f34408
j-33-t <prasenjeet.rathore@gmail.com>
2022-04-05 20:08:38 +0200

    add function

--- chapter_11_Project_2_Document.py
+++ chapter_11_Project_2_Document.py
@@ -2,19 +2,41 @@
 import cv2 as cv
 import numpy as np
 
+widthImg, heightImg = 640,480
+
 # ################    
 # # Using Webcam #
 # ################
 
 WebCam = cv.VideoCapture(0)
-WebCam.set(3,640) #width has id no. 3
-WebCam.set(4,480) #height has id no.4
+WebCam.set(3,widthImg) #width has id no. 3
+WebCam.set(4,heightImg) #height has id no.4
 WebCam.set(10,100) #brightness has id no.10
+
+
+def preProcessing(img):
+    # 1. Converting to greyscale 
+    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # 2. Apply Blur
+    imgBlur = cv.GaussianBlur(imgGray,(5,5),1)
+    
+    # Find Edges
+    imgCanny = cv.Canny(imgBlur, 200,200)
+    
+    # Dilation and Erosion
+    kernel = np.ones((5,5))
+    imgDialation = cv.dilate(imgCanny, kernel , iterations = 2)
+    imgThreshold = cv.erode(imgDialation, kernel, iterations = 1)
+    
+    return imgThreshold
 
 
 while True:
     success, img = WebCam.read()
     
+    # Resize width and height of img
+    cv.resize(img, (widthImg, heightImg))
     # Show result
     cv.imshow("WEBCAM", img)
     
